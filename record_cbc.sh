cb5a9a86ff75d40480f7860456574accb4842a91
Kevin Lucas <yu210148@gmail.com>
2016-08-27 08:40:51 -0700

    updated for new cbc stream url

--- record_cbc.sh
+++ record_cbc.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 rm -rf /var/www/html/cbc_recording/cbc.asf
-mplayer -playlist http://playerservices.streamtheworld.com/pls/CBC_R1_TOR_H.pls -dumpstream -dumpfile /var/www/html/cbc_recording/cbc.asf
+# commened out below line 2016-08-27 --KL
+#mplayer -playlist http://playerservices.streamtheworld.com/pls/CBC_R1_TOR_H.pls -dumpstream -dumpfile /var/www/html/cbc_recording/cbc.asf
+
 #mplayer http://14163.live.streamtheworld.com:80/CBC_R1_TOR_H_SC -dumpstream -dumpfile /var/www/cbc_recording/cbc.asf
 #mplayer http://6093.live.streamtheworld.com:80/CBC_R1_TOR_H_SC -dumpstream -dumpfile /var/www/cbc_recording/cbc.asf
+
+# on 2016-08-27 it appears that the cbc have changed their stream so I've commented
+# out the above line and put this one in pointing at their new stream --KL
+mplayer http://cbc_r1_tor.akacast.akamaistream.net/7/632/451661/v1/rc.akacast.akamaistream.net/cbc_r1_tor -dumpstream -dumpfile /var/www/html/cbc_recording/cbc.asf
